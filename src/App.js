94e7d213d20b15cb6512b92180c560083589560c
priyanshu pandey <prince678p@gmail.com>
2024-07-30 14:53:43 +0530

    redux will added to store search result

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
+import { Provider } from 'react-redux';
 import './App.css';
 import TypeHead from './TypeHead';
+import store from './store';
 
 function App() {
   return (
+  
     <div className="App">
+      <Provider store={store}>
       <TypeHead/>
+      </Provider>
     </div>
   );
 }
